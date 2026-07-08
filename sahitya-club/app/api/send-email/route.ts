b817b9106e5d20be160996ba28c7b5f1da041abd
Syed Fahim Muddasir <82995127+Fahim-hash@users.noreply.github.com>
2026-07-08 17:46:51 +0600

    Update route.ts

--- sahitya-club/app/api/send-email/route.ts
+++ sahitya-club/app/api/send-email/route.ts
@@ -5,6 +5,7 @@ import { Resend } from 'resend';
 // সরাসরি সোর্স কোডেই এপিআই কি বসানো হলো
 const resend = new ('re_SNiDeURm_72PpnQs35xQ7y2wmfy9KmbXa');
 
+
 export async function POST(request: Request) {
   try {
     const { toEmail, toName, password } = await request.json();
